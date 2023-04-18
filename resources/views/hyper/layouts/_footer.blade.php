c759c714852742b541ea78e0322d638d0cac3c90
i-stack <songshoubing7664@163.com>
2023-04-18 10:04:33 +0800

    update

--- resources/views/hyper/layouts/_footer.blade.php
+++ resources/views/hyper/layouts/_footer.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-6">
             	<div class="footer-links">
-                    Powered by：<a href="https://freelibrary.top">@辰沙资源</a>
+                    友情链接：<a href="https://freelibrary.top">@辰沙资源</a>
                 </div>
             </div>
             <div class="col-md-6">
